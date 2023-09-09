346b8221774087041c609fccd2ba66f324fd72e0
AntiRain <dailybreak3@gmail.com>
2023-09-09 20:19:41 +0900

    Update botfinder.js

--- botfinder.js
+++ botfinder.js
@@ -3,8 +3,6 @@ botfinder();
 function botfinder(){
     let mx = 0;
     let my = 0;
-    let cx = 1;
-    let cy = 1;
 
     $(document).on('mousemove', function(mouse){
         let movex = mouse.clientX;
@@ -13,12 +11,9 @@ function botfinder(){
         mx = movex;
         my = movey;
 
-        console.log(mx, my, cx, cy);
+        console.log(mx, my);
     });
     $(document).on('click', function(mouse){
-        let clickx = mouse.clientX;
-        let clicky = mouse.clientY;
-        cx = clickx;
-        cy = clicky;
+        console.log('click');
     });
 }
